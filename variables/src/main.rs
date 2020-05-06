2c58445e64b32a8819bc4237a07549dc82bc2797
wangyufan <wangyufan@chainup.com>
2020-05-06 16:28:58 +0800

    variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,22 @@
+const MAX_POINTS: u32 = 100_000;
 fn main() {
-    println!("Hello, world!");
+    println!("Constant MAX_POINTS is: {}", MAX_POINTS);
+
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+
+    println!("The value of x is: {}", x);
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("The space length is: {}", spaces)
+
 }
