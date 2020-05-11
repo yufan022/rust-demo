39e6ae92e2d62ba12d966c6b41707188bc85e137
wangyufan <wangyufan@chainup.com>
2020-05-11 10:36:06 +0800

    function

--- function/src/main.rs
+++ function/src/main.rs
@@ -1,9 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    another_function(5, 6);
 
-    another_function();
+    let x = 5;
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {}", y);
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
 }
