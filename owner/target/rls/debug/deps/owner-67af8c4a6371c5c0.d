7b278fc221bbd32bdb23a050cc7cfd7a12b8a9fe
wangyufan <wangyufan@chainup.com>
2020-05-18 20:23:13 +0800

    owner

---
+++ owner/target/rls/debug/deps/owner-67af8c4a6371c5c0.d
@@ -0,0 +1,5 @@
+/Users/yufan/develop/my-open-source/rust/rust-demo/owner/target/rls/debug/deps/owner-67af8c4a6371c5c0.rmeta: src/main.rs
+
+/Users/yufan/develop/my-open-source/rust/rust-demo/owner/target/rls/debug/deps/owner-67af8c4a6371c5c0.d: src/main.rs
+
+src/main.rs:
