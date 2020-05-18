92c3d0755b91feed5cab65efab4d8a3dedf0a986
wangyufan <wangyufan@chainup.com>
2020-05-18 20:49:52 +0800

    reference

--- owner/src/main.rs
+++ owner/src/main.rs
@@ -91,7 +91,7 @@ fn takes_and_gives_back(a_string: String) -> String { // a_string 进入作用�
     a_string  // 返回 a_string 并移出给调用的函数
 }
 
-fn main() {
+fn main8() {
     let s1 = String::from("hello");
 
     let (s2, len) = calculate_length(s1);
@@ -104,3 +104,14 @@ fn calculate_length(s: String) -> (String, usize) {
 
     (s, length)
 }
+
+fn main(){
+    let s1 = String::from("hello");
+    let len = calculate_length1(&s1);
+    
+}
+
+fn calculate_length1(s: &String) -> usize { // s 是对 String 的引用
+    s.len()
+} // 这里，s 离开了作用域。但因为它并不拥有引用值的所有权，
+  // 所以什么也不会发生
