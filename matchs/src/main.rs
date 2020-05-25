ec9e6f2d69464a3416c782443b8fdb824f1ad93d
wangyufan <wangyufan@chainup.com>
2020-05-25 14:42:29 +0800

    matches

--- matchs/src/main.rs
+++ matchs/src/main.rs
@@ -14,7 +14,10 @@ fn main() {
         7 => println!("seven"),
         _ => (),
     }
-
+    let some_u8_value = Some(3u8);
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
 
 }
 
