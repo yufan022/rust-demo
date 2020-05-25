d3e8ba5224039df14972f01e978645cff4d1fad9
wangyufan <wangyufan@chainup.com>
2020-05-25 19:10:39 +0800

    split mode

---
+++ split_mode/src/lib.rs
@@ -0,0 +1,16 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
